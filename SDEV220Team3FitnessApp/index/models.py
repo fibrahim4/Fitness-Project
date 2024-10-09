b8e737f08d4c1142d5625da1c092343c30b50d00
PK325 <157205316+PK325@users.noreply.github.com>
2024-10-09 03:50:38 -0400

    Update models.py

--- SDEV220Team3FitnessApp/index/models.py
+++ SDEV220Team3FitnessApp/index/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class WorkoutPlan(models.Model):
+    workout_name = models.CharField(max_length=100)
+    exercise = models.CharField(max_length=100)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    duration = models.IntegerField()  # Duration in minutes
+
+    def __str__(self):
+        return self.workout_name
