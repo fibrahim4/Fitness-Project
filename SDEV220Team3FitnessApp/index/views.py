375e8a88103e1ceee53edcb630186ef4b7fabb2c
PK325 <157205316+PK325@users.noreply.github.com>
2024-10-09 03:37:00 -0400

    Update views.py

--- SDEV220Team3FitnessApp/index/views.py
+++ SDEV220Team3FitnessApp/index/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import WorkoutPlanForm
 
 def index(request):
     return render(request, 'index/index.html')
 
+def create_workout_plan(request):
+    if request.method == 'POST':
+        form = WorkoutPlanForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the form data to the database
+            return redirect('index')  # Redirect to index page or another page
+    else:
+        form = WorkoutPlanForm()
+
+    return render(request, 'index/create_workout_plan.html', {'form': form})
